cbcf39fdac5ddf163b00d4a55b645646cf9e5922
wkuziow <wkuziow@gmail.com>
2021-01-15 21:48:39 +0100

    address entity

---
+++ src/main/java/pl/kuziow/mobileappwebservices/io/entity/AddressEntity.java
@@ -0,0 +1,8 @@
+package pl.kuziow.mobileappwebservices.io.entity;
+
+import java.io.Serializable;
+
+public class AddressEntity implements Serializable {
+
+    private static final long serialVersionUID = -7248001367511771931L;
+}
