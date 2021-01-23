142b9918e3317a05e75bf784b9ee271dd36674d1
wkuziow <wkuziow@gmail.com>
2021-01-23 14:31:10 +0100

    email verification

--- src/main/java/pl/kuziow/mobileappwebservices/security/WebSecurity.java
+++ src/main/java/pl/kuziow/mobileappwebservices/security/WebSecurity.java
@@ -24,7 +24,10 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
+                .permitAll()
+                .antMatchers(HttpMethod.GET, SecurityConstants.VERIFICATION_EMAIL_URL)
                 .permitAll()
                 .anyRequest().authenticated().and()
                 .addFilter(getAuthenticationFilter())
